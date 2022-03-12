1fbdce467378ffa880d69d6d8fb7e8c10ad7ec20
Roy Dyck <dyck.ro@gmail.com>
2022-03-12 16:18:41 -0600

    Write for-loop to generate drawing grid

---
+++ script.js
@@ -0,0 +1,21 @@
+const scratchpad = document.querySelector('.scratchpad');
+
+const ROWS = 100;
+const columns = ROWS;
+
+
+
+
+
+
+for (let i = 0; i < ROWS; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    scratchpad.appendChild(row);
+
+    for (let j = 0; j < columns; j++) {
+        const cell = document.createElement('cell');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+}
